5364febafaab760b751f19ba0c3a35b92f6f5076
galle997 <98421465+galle997@users.noreply.github.com>
2022-06-12 18:51:35 -0600

    se agrega pagina de home al frontend

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="app">
-        <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-          <b-collapse id="nav-collapse" is-nav>
-           <b-navbar-nav>
-             <b-nav-item href="#">
-                <router-link to="/">
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-toggle target="navbar-toggle-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+          <b-icon v-else icon="chevron-bar-down"></b-icon>
+        </template>
+      </b-navbar-toggle>
+      <b-navbar-brand href="#">
+        <router-link to="/">
+                <img id="logohome" src="../src/assets/Logos/white_logo_transparent_backgroundHorizontal.png" 
+                alt="" class="mr-sm-2" width="60px" height="40px" center>
                 </router-link> 
-              </b-nav-item>
-              <b-nav-item href="#" class="mr-sm-6">
-              </b-nav-item>
-           </b-navbar-nav>
-      <!-- Right aligned nav items -->
-      <b-navbar-nav>
-        <b-nav-item-dropdown text="Integrantes:" right>
-          <b-dropdown-item href="#">David Alexis Gallegos González</b-dropdown-item>
-          <b-dropdown-item href="#">Nestor Wilfredo Chavez Ramirez</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+      </b-navbar-brand>
+
+
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Merch</b-nav-item>
+          <b-nav-item href="#">Iniciar Sesión</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view></router-view>
   </div>
 </template>
@@ -31,18 +34,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,127 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-carousel id="carousel-1" v-model="slide" :interval="4000" controls indicators background="#ababab"
+      img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd">
+      <!-- Slides with custom text -->
+      <b-carousel-slide
+        img-src="https://www.esdesignbarcelona.com/sites/default/files/inline-images/Que%CC%81%20es%20el%20estilo%20de%20ropa%20aesthetic%2C%20la%20moda%20de%20la%20que%20todos%20hablan%20%282%29.png"
+        caption="¡Bienvenido a:">
+        <h1>Hakoo Store!</h1>
+        <b-button pill variant="outline-danger">Registrar</b-button>
+      </b-carousel-slide>
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        img-src="https://www.esdesignbarcelona.com/sites/default/files/2022-03/Que%CC%81%20es%20el%20estilo%20de%20ropa%20aesthetic%2C%20la%20moda%20de%20la%20que%20todos%20hablan%20%283%29_0.png">
+        <h1>¡Aprovecha los beneficios!</h1>
+        <b-button pill variant="outline-danger">Registrar</b-button>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <img class="d-block img-fluid w-100" width="1024" height="480"
+            src="https://www.esdesignbarcelona.com/sites/default/files/inline-images/Que%CC%81%20es%20el%20estilo%20de%20ropa%20aesthetic%2C%20la%20moda%20de%20la%20que%20todos%20hablan.png"
+            alt="image slot">
+        </template>
+        <h1>Registrate y ponte a la moda</h1>
+        <b-button pill variant="outline-danger">Registrar</b-button>
+      </b-carousel-slide>
+    </b-carousel>
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Conéctate con nosotros:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Hakoo Store S.A de C.V
+          </h6>
+          <p>
+            Con 30 años de experiencia vendiendo  lo más moderno en ropa y accesorios, te 
+            acompañamos en cada paso que das.
+          </p>
+        </div>
+        <!-- Grid column -->
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Trabaja con Nosotros
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Envía tu currículum</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-3 col-xl-4 mx-auto mb-md-0 mb-5">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contacto:
+          </h6>
+          <p><i class="fas fa-home me-4"></i>San Miguel,El Salvador,59AVSur,Calle el Progreso</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            servicioalcliente@hakoostore.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +503 2345-6789</p>
+          <p><i class="fas fa-print me-3"></i> +503 2219-7860</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://github.com/galle997/Proyecto_Hakoo">Hakoo Store Project</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+    <HelloWorld msg="Hakoo" />
   </div>
 </template>
 
@@ -13,6 +133,21 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true
+    },
+    onSlideEnd() {
+      this.sliding = false
+    }
   }
 }
+
 </script>
