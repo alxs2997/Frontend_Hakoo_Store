83a72679917cf229ed33eaaa3515e24e16ce2682
galle997 <98421465+galle997@users.noreply.github.com>
2022-06-13 15:06:41 -0600

    añadiendo modal a Home

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,43 @@
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="/">Home</b-nav-item>
           <b-nav-item href="#">Merch</b-nav-item>
-          <b-nav-item href="/Login">Iniciar Sesión</b-nav-item>
+          <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')">Iniciar Sesión</b-button>
+          <!-- Modal accion formulario  -->
+          <b-modal id="bv-modal-example" hide-footer class="modal fade" role="dialog">
+            <div class="modal-content">
+              <template class="modal-header">
+                  <h2 text="center">¡Hola de Nuevo!</h2>
+                  <h4 text="center">Bienvenido/a</h4>
+                  <img id="logohome" src="../src/assets/Logos/white_logo_transparent_backgroundHorizontal.png" alt=""
+                  class="mr-sm-2" width="60px" height="40px" center>
+              </template>
+              <!-- Modal Body -->
+              <div class="modal-body">
+                <p class="statusMsg"></p>
+                <b-form-group role="form">
+                  <div class="form-group">
+                    <label for="inputUser">Usuario:</label>
+                    <b-input type="text" class="form-control" id="inputName" placeholder="Ingresa tu correo o usuario...">
+                    </b-input>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputpass">Contraseña:</label>
+                    <b-input type="password" name="password" class="form-control" id="inputEmail" placeholder="Enter your email">
+                    </b-input>
+                  </div>
+                </b-form-group>
+              </div>
+
+              <!-- Modal Footer -->
+              <div class="modal-footer">
+                <b-link href="/">Cancelar</b-link>
+                |
+                <b-button pill variant="outline-secondary" href="/Registro">Registrarse</b-button>
+                <b-button pill variant="outline-info">Iniciar Sesión</b-button>
+              </div>
+            </div>
+          </b-modal>
+          <!-- Modal Accion formulario  -->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
