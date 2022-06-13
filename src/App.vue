0bb6419a1156dab2c752c12164669b48fd8a0217
galle997 <98421465+galle997@users.noreply.github.com>
2022-06-13 17:41:46 -0600

    Se Agrega modal de carrito

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,27 @@
           <img id="logohome" src="../src/assets/Logos/white_logo_transparent_backgroundHorizontal.png" alt=""
             class="mr-sm-2" width="60px" height="40px" center>
         </router-link>
+        |
+        <button type="button" class="btn btn-outline-secondary position-relative" id="show-btn" @click="$bvModal.show('bv-modal-exe')">
+          <i class="fa fa-cart-plus"></i>
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            7
+            <span class="visually-hidden">Sus productos añadidos</span>
+          </span>
+        </button>
+        <b-modal id="bv-modal-exe" hide-footer>
+          <template #modal-title>
+              <h2 text="center">Productos Añadidos</h2>
+            </template>
+            <div class="d-block text-center">
+              +
+            </div>
+            <b-button pill variant="outline-secondary" class="mt-3" block @click="$bvModal.hide('bv-modal-exe')">
+              Eliminar</b-button>
+            <div class="modal-footer">
+              <b-button pill variant="outline-success">Pagar</b-button>
+            </div>
+        </b-modal>
       </b-navbar-brand>
 
 
@@ -19,7 +40,8 @@
         <b-navbar-nav class="ml-auto">
           <b-button squared variant="outline-secondary" href="/">Home</b-button>
           <b-button squared variant="outline-secondary" href="#">Merch</b-button>
-          <b-button squared variant="outline-secondary" id="show-btn" @click="$bvModal.show('bv-modal-example')">Iniciar Sesión</b-button>
+          <b-button squared variant="outline-secondary" id="show-btn" @click="$bvModal.show('bv-modal-example')">Iniciar
+            Sesión</b-button>
           <!-- Modal accion formulario  -->
           <b-modal id="bv-modal-example" hide-footer>
             <template #modal-title>
@@ -28,25 +50,26 @@
             <div class="d-block text-center">
               <h3>Ingrese sus credenciales</h3>
               <b-form-group role="form">
-                  <div class="form-group">
-                    <label for="inputUser">Usuario:</label>
-                    <b-input type="text" class="form-control" id="inputName" placeholder="Ingresa tu correo o usuario...">
-                    </b-input>
-                  </div>
-                  <br>
-                  <div class="form-group">
-                    <label for="inputpass">Contraseña:</label>
-                    <b-input type="password" name="password" class="form-control" id="inputPass" placeholder="Password">
-                    </b-input>
-                  </div>
-                </b-form-group>
+                <div class="form-group">
+                  <label for="inputUser">Usuario:</label>
+                  <b-input type="text" class="form-control" id="inputName" placeholder="Ingresa tu correo o usuario...">
+                  </b-input>
+                </div>
+                <br>
+                <div class="form-group">
+                  <label for="inputpass">Contraseña:</label>
+                  <b-input type="password" name="password" class="form-control" id="inputPass" placeholder="Password">
+                  </b-input>
+                </div>
+              </b-form-group>
             </div>
-            <b-button pill variant="outline-secondary" class="mt-3" block @click="$bvModal.hide('bv-modal-example')">Cancelar</b-button>
+            <b-button pill variant="outline-secondary" class="mt-3" block @click="$bvModal.hide('bv-modal-example')">
+              Cancelar</b-button>
             <div class="modal-footer">
               <b-button pill variant="secondary" href="/Registro">Registrarse</b-button>
-                <b-button pill variant="outline-info">Iniciar Sesión</b-button>
+              <b-button pill variant="outline-info">Iniciar Sesión</b-button>
             </div>
-            
+
           </b-modal>
           <!-- Modal Accion formulario  -->
         </b-navbar-nav>
